8fba08aa890fb32ec49330c8fd93830474a56e1d
David Ladowitz <david@ladowitz.com>
2016-03-01 12:39:17 -0800

    updating new user action when failed create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
     if params[:agent_id]
       @agent = User.find params[:agent_id]
       @user = @agent.clients.new
+    elsif @user #if rendering from a failed create
+      @agent = @user.agent
     else
       @user = User.new
       @agents = User.agents.order(:first_name)
